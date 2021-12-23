dbdcd9e7d094603d7a0811aebf3e7b2f77dd7ab0
DanelionBAD <dandelionbad@gmail.com>
2021-12-23 16:21:07 +0100

    +aggiunta dockerizzazione

---
+++ foodnetprog/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCvAOfzum_j8WyVvgCgVVyJ6nrO8uuZI_c",
+  authDomain: "programmazione-ii.firebaseapp.com",
+  projectId: "programmazione-ii",
+  storageBucket: "programmazione-ii.appspot.com",
+  messagingSenderId: "29517220450",
+  appId: "1:29517220450:web:7f463fa02b1ad6d7715f3b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
